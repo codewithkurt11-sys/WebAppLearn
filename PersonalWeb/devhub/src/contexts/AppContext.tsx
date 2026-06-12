334a15cbea8d9804df86f1e8d27fcf458ff83850
codewithkurt11-sys <codewithkurt11gmail.com>
2026-06-12 09:10:17 +0800

    fix0

--- PersonalWeb/devhub/src/contexts/AppContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from "react";
-import type { AppCtxType } from "../types";
-
-export const AppCtx = createContext<AppCtxType | null>(null);
-export const useApp = () => useContext(AppCtx)!;
-export const RunCtx = createContext<boolean>(true);
